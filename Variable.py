a18bf9a972a7764516332e4082043e115b805ad5
NirmalaSubramani2001 <nirmalalogu01@gmail.com>
2025-11-27 19:37:48 +0530

    Updated variable.py file

---
+++ Variable.py
@@ -0,0 +1,21 @@
+# a=int(input("1. Enter a number"))
+# b=int(input("2. Enter a number"))
+# c=a+b
+# print (c)
+
+# l=[1,2,3,4,5]
+# for i in l:
+#     print (i)
+
+# l=[1,2,3,4,5]
+# for i in l:
+#    print (i, end = "")
+
+
+l=[1,2,3,4,5]
+l[3] = 10
+for i in l:
+   print (i)
+
+
+
